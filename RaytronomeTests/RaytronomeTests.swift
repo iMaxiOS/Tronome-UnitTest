8cbcdff63be4967e574112085c4f54cf09289734
m.granchenko <m.granchenko@simcord.com>
2018-12-27 15:49:15 +0200

    test numerator

--- RaytronomeTests/RaytronomeTests.swift
+++ RaytronomeTests/RaytronomeTests.swift
@@ -67,6 +67,153 @@ class RaytronomeTests: XCTestCase {
         ])
     }
     
+    func testModifyingNumeratorUpdatesNumeratorText() {
+        let numerator = scheduler.createObserver(String.self)
+        
+        viewModel.numeratorText
+            .drive(numerator)
+            .disposed(by: disposeBag)
+        
+        scheduler.createColdObservable([.next(10, 3),
+                                        .next(15, 1)])
+            .bind(to: viewModel.steppedNumerator)
+            .disposed(by: disposeBag)
+        
+        scheduler.start()
+        
+        XCTAssertEqual(numerator.events, [
+            .next(0, "4"),
+            .next(10, "3"),
+            .next(15, "1")
+            ])
+    }
+    
+    func testModifyingDenominatorUpdatesNumeratorText() {
+        let denominator = scheduler.createObserver(String.self)
+        
+        viewModel.denominatorText
+            .drive(denominator)
+            .disposed(by: disposeBag)
+        
+        // Denominator is 2 to the power of `steppedDenominator`.
+        // f(1, 2, 3, 4) = 4, 8, 16, 32
+        scheduler.createColdObservable([.next(10, 2),
+                                        .next(15, 4),
+                                        .next(20, 3),
+                                        .next(25, 1)])
+            .bind(to: viewModel.steppedDenominator)
+            .disposed(by: disposeBag)
+        
+        scheduler.start()
+        
+        XCTAssertEqual(denominator.events, [
+            .next(0, "4"),
+            .next(10, "8"),
+            .next(15, "32"),
+            .next(20, "16"),
+            .next(25, "4")
+            ] )
+    }
+    
+    func testModifyingTempoUpdatesTempoText() {
+        let tempo = scheduler.createObserver(String.self)
+        
+        viewModel.tempoText
+            .drive(tempo)
+            .disposed(by: disposeBag)
+        
+        scheduler.createColdObservable([.next(10, 75),
+                                        .next(15, 90),
+                                        .next(20, 180),
+                                        .next(25, 60)])
+            .bind(to: viewModel.tempo)
+            .disposed(by: disposeBag)
+        
+        scheduler.start()
+        
+        XCTAssertEqual(tempo.events, [
+            .next(0, "120 BPM"),
+            .next(10, "75 BPM"),
+            .next(15, "90 BPM"),
+            .next(20, "180 BPM"),
+            .next(25, "60 BPM")
+            ])
+    }
+    
+    func testModifyingSignatureUpdatesSignatureText() {
+        let signature = scheduler.createObserver(String.self)
+        
+        viewModel.signatureText
+            .drive(signature)
+            .disposed(by: disposeBag)
+        
+        scheduler.createColdObservable([.next(5, 3),
+                                        .next(10, 1),
+                                        .next(20, 5),
+                                        .next(25, 7),
+                                        .next(35, 12),
+                                        .next(45, 24),
+                                        .next(50, 32)])
+            
+            .bind(to: viewModel.steppedNumerator)
+            .disposed(by: disposeBag)
+        
+        scheduler.createColdObservable([.next(15, 2),
+                                        .next(30, 3),
+                                        .next(40, 4)])
+            .bind(to: viewModel.steppedDenominator)
+            .disposed(by: disposeBag)
+        
+        scheduler.start()
+        
+        XCTAssertEqual(signature.events, [
+            .next(0, "4/4"),
+            .next(5, "3/4"),
+            .next(10, "1/4"),
+            .next(15, "1/8"),
+            .next(20, "5/8"),
+            .next(25, "7/8"),
+            .next(30, "7/16"),
+            .next(35, "12/16"),
+            .next(40, "12/32"),
+            .next(45, "24/32"),
+            .next(50, "32/32")
+        ])
+    }
+    
+    func testModifyingDenominatorUpdatesNumeratorValueIfExceedsMaximum() {
+        let numerator = scheduler.createObserver(Double.self)
+        
+        viewModel.numeratorValue
+            .drive(numerator)
+            .disposed(by: disposeBag)
+        
+        // Denominator is 2 to the power of `steppedDenominator`.
+        // f(1, 2, 3, 4) = 4, 8, 16, 32
+        scheduler.createColdObservable([.next(5, 4), // switch to 32nds
+                                        .next(15, 3), // switch to 16ths
+                                        .next(20, 2), // switch to 8ths
+                                        .next(25, 1)  // switch to 4ths
+            ])
+            
+            .bind(to: viewModel.steppedDenominator)
+            .disposed(by: disposeBag)
+        
+        scheduler.createColdObservable([.next(10, 24)])
+            .bind(to: viewModel.steppedNumerator)
+            .disposed(by: disposeBag)
+        
+        scheduler.start()
+        
+        XCTAssertEqual(numerator.events, [.next(0, 4), // Expected to be 4/4
+                                        .next(10, 24), // Expected to be 24/32
+                                        .next(15, 16), // Expected to be 16/16
+                                        .next(20, 8), // Expected to be 8/8
+                                        .next(25, 4) // Expected to be 4/4
+        ])
+    }
+
+    
     
     
 }
